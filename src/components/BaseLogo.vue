d862c89590303a5d2b7795fd1c72463df8c83b16
Tmisatoy <luck.m.1117@gmail.com>
2020-02-04 03:53:00 +0900

    Add: ロゴの基底コンポーネントを作成

---
+++ src/components/BaseLogo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="logo">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    default: String
+  }
+};
+</script>
+
+<style scoped>
+.logo {
+  font-size: 60px;
+}
+</style>
