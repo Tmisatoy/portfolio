75f1241f83a9fea5ca1589ac552a242dd1f2b7c8
Tmisatoy <luck.m.1117@gmail.com>
2020-02-04 03:07:41 +0900

    Add: routerファイル、またはroutesを追加

--- src/router.js
+++ src/router.js
@@ -1,8 +1,45 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
+import Top from "./views/Top.vue";
+import Works from "./views/Works.vue";
+import Skills from "./views/Skills.vue";
+import About from "./views/About.vue";
+import Blog from "./views/Blog.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
-new Router({
-    routes: [{path: '/', component: },{}]
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      components: {
+        default: Top
+      }
+    },
+    {
+      path: "/works",
+      components: {
+        default: Works
+      }
+    },
+    {
+      path: "/skills",
+      components: {
+        default: Skills
+      }
+    },
+    {
+      path: "/about",
+      components: {
+        default: About
+      }
+    },
+    {
+      path: "/blog",
+      components: {
+        default: Blog
+      }
+    }
+  ]
 });
