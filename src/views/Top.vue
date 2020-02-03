ae3ce78b8b3392c77ee5214d34fd2983487bb77f
Tmisatoy <luck.m.1117@gmail.com>
2020-02-04 03:51:30 +0900

    Add: ページコンポーネント、viewsを追加
    router-viewで使用する５ページ分全てのページコンポーネントを追加

---
+++ src/views/Top.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="content">
+    <base-logo>
+      Misato Todaka's
+      <br />Portfolio
+    </base-logo>
+  </main>
+</template>
+
+<script>
+import BaseLogo from "../components/BaseLogo.vue";
+export default {
+  components: {
+    BaseLogo
+  }
+};
+</script>
+
+<style scoped>
+</style>
