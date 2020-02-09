65a1cb5604d07d1dcb7d2a679ab8b7dcc964a21b
Tmisatoy <luck.m.1117@gmail.com>
2020-02-10 03:10:16 +0900

    Add:Works.vueを追加

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,3 +1,22 @@
 <template>
-  <div></div>
+  <main class="main-container">
+    <div class="main-area">
+      <base-logo>
+        Works
+        <br />
+      </base-logo>
+    </div>
+    <div class="contact-area bands-container">
+      <h2>Contact</h2>
+    </div>
+  </main>
 </template>
+
+<script>
+import BaseLogo from "../components/BaseLogo.vue";
+export default {
+  components: {
+    BaseLogo
+  }
+};
+</script>
