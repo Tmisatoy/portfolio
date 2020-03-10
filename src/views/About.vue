61a865f968a8c9e24a8f81f9afe84835a82f9efb
Tmisatoy <luck.m.1117@gmail.com>
2020-03-11 01:56:06 +0900

    Add: profile追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,23 @@
 <template>
   <main class="main-container">
-    <div class="main-area">
-      <BaseLogo>
+    <div class="main-area--all">
+      <BaseLogo class="grid-item--top">
         About
         <br />
       </BaseLogo>
+      <div class="main-area__about-content grid-item--top">
+        <div class="main-area__about-content__heading">戸高 美里</div>
+        <span>とだか みさと</span>
+        <dl>
+          <dt>最終学歴</dt>
+          <dd>N高等学校</dd>
+          <dt>出身地</dt>
+          <dd>宮崎県</dd>
+          <dt>趣味</dt>
+          <dd>服作り、美術館巡り、Youtubeを観ること</dd>
+        </dl>
+      </div>
     </div>
-    <div class="bands-container"></div>
   </main>
 </template>
 
@@ -15,6 +26,45 @@ import BaseLogo from "../components/BaseLogo.vue";
 export default {
   components: {
     BaseLogo
+  },
+  data() {
+    return {
+      item: [{ title: "最終学歴", txt: "N高等学校" }, {}]
+    };
   }
 };
 </script>
+
+<style scoped lang="scss">
+.main-area__about-content__heading {
+  font-size: 40px;
+}
+
+.main-area__about-content span {
+  font-size: 14px;
+  color: #888;
+  letter-spacing: 12px;
+}
+
+.main-area__about-content dl {
+  width: 600px;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-rows: auto;
+  grid-template-columns: 15% 85%;
+}
+
+.main-area__about-content dt {
+  color: #888;
+
+  line-height: 30px;
+  width: 100%;
+  border: 1px solid #888;
+  text-align: center;
+}
+
+.main-area__about-content dd {
+  line-height: 30px;
+  margin-left: 10px;
+}
+</style>
